ad5df9ba5a09573928dd970a30955b760d510125
apascualco <apascualco@gmail.com>
2024-05-18 15:24:57 +0200

    Add first endpoint to test how do it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn first_endpoint() -> impl Responder {
+    return "First endpoint!!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(first_endpoint)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
